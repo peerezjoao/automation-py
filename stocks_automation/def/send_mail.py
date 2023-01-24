ce5af411fc79c43b1e95a90b1e5e3fbdb3c8f36f
João Perez <peerezjoao1@gmail.com>
2023-01-23 22:57:45 -0300

    send mail 2.0

--- stocks_automation/def/send_mail.py
+++ stocks_automation/def/send_mail.py
@@ -1,17 +1,27 @@
-import email, smtplib, ssl
+import smtplib
+from os.path import basename
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
-from email import encoders
-from email.mine.base import MINEBase
-from email.mine.multipart import Mi
+from_addr = 'echobotuipath@gmail.com'
+to_addr = 'peerezjoao1@gmail.com'
+subject = 'Relatório Gerencial disponível para download: '
+content = 'Segue o relatório gerencial do fundo:'
 
+msg = MIMEMultipart()
+msg['From'] = from_addr
+msg['To'] = to_addr
+msg['Subject'] = subject
+body = MIMEText(content, 'plain')
+msg.attach(body)
 
+filename = "C:\Users\peere\Downloads\DAS-PGMEI-44395425000112-AC2022 (1).pdf"
+with open(filename, 'r') as f:
+    attachment = MIMEApplication(f.read(), Name=basename(filename))
+    attachment['Content-Disposition'] - 'attachment; filename-"{}'.format(basename(filename))
+    
+    
+msg.attach(attachment)
 
-port = 465
-
-PASSWORD = ''
-email = 'peerezjoao1@gmail.com'
-
-context = ssl.create_default_context()
-
-with smtplib.SMTP_SSL('smtp.gmail.com', port, context=context) as server:
-    server.login(email, password)
+server = smtplib.SMTP('smtp.dreamhost.com',587)
+server.login(from_addr, 'mystrongpw!')
